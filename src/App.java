05c33ce46306945f116e5c95d5f1b4c4abb0b8de
ngkhanggg <nguyenkhangchu2004@gmail.com>
2022-03-01 17:33:34 -0500

    removed throws exception

--- src/App.java
+++ src/App.java
@@ -1,8 +1,5 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        // new Frame();
-        Game game = new Game();
-        int lines = game.countLines();
-        System.out.println(lines);
+    public static void main(String[] args) {
+        new Frame();
     }
 }
